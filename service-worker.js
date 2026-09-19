fccb32ddbd7fa6001050b9f732bca04c912e4c95
wibem1 <wibem1@googlemail.com>
2026-09-19 03:38:09 +0200

    v0.4.22 disable stale PWA caching

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,4 @@
-const CACHE_NAME='minimal-composer-v0.4.21';
-const APP_SHELL=['./','./index.html','./manifest.webmanifest','./minimal-icon-180.png','./minimal-icon-192.png','./minimal-icon-512.png','./icon.svg'];
-self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting()});
-self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))))});
+const CACHE_NAME='minimal-composer-v0.4.22';
+self.addEventListener('install',e=>{self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}))});
