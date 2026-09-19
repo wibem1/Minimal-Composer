a40d846b3dc36167012c81a907a35b52e7de3580
wibem1 <wibem1@googlemail.com>
2026-09-19 03:31:37 +0200

    v0.4.20 add PWA service worker

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE_NAME='minimal-composer-v0.4.20';
+const APP_SHELL=['./','./index.html','./manifest.webmanifest','./minimal-icon-180.png','./minimal-icon-192.png','./minimal-icon-512.png','./icon.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting()});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))))});
