3e9a88a6d83a2082c04cea7cd07a81e717bbfdb8
wibem1 <wibem1@googlemail.com>
2026-09-20 21:43:34 +0200

    v0.5.11 – Cachekennung setzen

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='minimal-composer-modular-v0.5.0';
+const CACHE_NAME='minimal-composer-v0.5.11';
 self.addEventListener('install',e=>{self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}))});
